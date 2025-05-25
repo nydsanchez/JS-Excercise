28260d304a953245d280428fcdf94e3626c50035
Nydia Massiel Sánchez <nydsanchez@gmail.com>
2025-05-24 21:48:39 -0600

    Separar html de js - ternario

---
+++ Condicionales/ternario.js
@@ -0,0 +1,10 @@
+let result = a + b < 4 ? "Debajo" : "Encima";
+
+let message =
+  login == "Empleado"
+    ? "Hola"
+    : login == "Director"
+    ? "Felicidades"
+    : login == ""
+    ? "Sin sesion"
+    : "";
